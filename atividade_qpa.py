ddf70f26d58484925d39c274b2180b36e3a7b5b3
iago <xxiagointer@gmail.com>
2021-05-13 10:45:08 -0300

    fix cost function for asymetric matrix, clean some comments and add results

--- atividade_qpa.py
+++ atividade_qpa.py
@@ -2,17 +2,13 @@
 
 """**QPA**"""
 
-# Commented out IPython magic to ensure Python compatibility.
 import math
 import secrets
 import numpy as np
 import os
 import subprocess
-#import time
 from matplotlib import pyplot as plt
-
 # %matplotlib inline
-
 
 def load_matrices(path_to_file):
   #file format:
@@ -43,36 +39,17 @@ def load_matrices(path_to_file):
         distance_size += 1
   raw_numbers.close()
   nF, nD = (np.array(F), np.array(D))
-      
-  print(nF.shape)
-  print(nD.shape)
-  
   return (matrix_size, nF, nD )
 
-
-def permutation(n, index_array, Matrix):
-  #index_array                            #Flux Matrix index duple vector
-  #[3,0,2,1] <- makes this permutation -> [(3 0) (3 2) (3 1) (0 2) (0 1) (2 1)]
-  #        Matrix positional value        [ x,    y,    z,    w,    k,    v ]
-  res =[]
-  for i in range(n-1):
-    for j in range(i+1, n):
-      #print("("+ str(index_array[i])+ "," +str(index_array[j]) +")")
-      res.append( Matrix[index_array[i]][index_array[j]] )
-  return np.array(res)
-
-
-
-def cost_function(flux_index_matrix, dip, n, F):
-  flux_matrix_pos_value =  permutation(n, flux_index_matrix, F)    
-  return  np.dot(dip, flux_matrix_pos_value)
-
-
+def cost_function(n, solution, F, D):
+    cost=0
+    for i in range(n):
+        for j in range(n):
+            cost += D[solution[i], solution[j]] * F[i][j]
+    return cost
 
 """**ITERATE LOCAL SEARCH**
-
 """
-
 #Procedure ILS
 # s0 = GeneralInitialSolution
 # s* = LocalSearch(s0)
@@ -83,14 +60,12 @@ def cost_function(flux_index_matrix, dip, n, F):
 # while condition
 # end
 
-
 def swap(a,b,vet):
   c = vet[a]
   vet[a] = vet[b]
   vet[b] = c
 
-
-def local_search(n, history, s_vector, dip, F):
+def local_search(n, history, s_vector, D ,F):
   cost = [] # cost of neighbors
   first = 0
   #find the first, this one will be the index for maping the neighbors
@@ -107,7 +82,7 @@ def local_search(n, history, s_vector, dip, F):
     if history[i] == 1 :
        neighbor = s_vector.copy()
        swap(first,i,neighbor)
-       value = cost_function(neighbor, dip, n, F)
+       value = cost_function(n, neighbor, F, D)
        cost.append(value)
        if min(cost) == value:  #keep track of the minimum cost's position
          index_min_cost = i
@@ -116,32 +91,28 @@ def local_search(n, history, s_vector, dip, F):
   min_cost = min(cost)
   neighbor = s_vector.copy()
   swap(first, index_min_cost, neighbor)
-  #making new history
   new_history = history.copy()
   new_history[first]=0
   new_history[index_min_cost]=0
-  return (new_history, min_cost, neighbor) # first, index_min_cost, 
+  return (new_history, min_cost, neighbor)
 
-
-def perturbation(n, history, s_vector, dip, F):
+def perturbation(n, history, s_vector, D, F):
   indexes = [i for i in range(n) if history[i] == 1]
-  #perturbation is a swap of two(or more) random positions
   a = secrets.choice(indexes)
   indexes.remove(a) # a != b
   b = secrets.choice(indexes)
   new_s = s_vector.copy()
   swap(a,b, new_s)
-  new_cost = cost_function(new_s, dip, n , F)    
+  new_cost = cost_function(n, new_s, F, D)    
   new_history = history.copy()
-  #new_history[a] =0
-  #new_history[b] =0
   return (new_history, new_cost, new_s)
-
 
 def better(sa, sa1):
   return sa if sa[1] < sa1[1] else sa1
+
 def rw(sa, sa1):
   return sa1
+
 def lsmc(sa, sa1, history):
   if sa1[1] < sa[1] :
     return sa1
@@ -151,7 +122,6 @@ def lsmc(sa, sa1, history):
     pv = [1-psa1, psa1]
     return sa if np.random.choice([0,1], 1, pv)[0] == 0 else sa1
 
-
 def acceptance_criterion(sa, sa1, history, flag):
   if flag == 0:
     return better(sa,sa1)
@@ -160,25 +130,21 @@ def acceptance_criterion(sa, sa1, history, flag):
   else:
     return lsmc(sa, sa1, history)  
 
-
-def ils(n,dip,F, criteria_flag):
+def ils(n, D, F, criteria_flag):
   list_plot=[]
-
   history = [1 for i in range(n)] # history is nothing but a bit map
   s0 = [i for i in range(n)] #s identity is the general solution
-  sa = local_search(n, history, s0, dip, F)
+  sa = local_search(n, history, s0, D, F)
   #do
-  s1 = perturbation(n, sa[0], sa[2], dip, F)
-  sa1 = local_search(n, s1[0], s1[2], dip, F)
+  s1 = perturbation(n, sa[0], sa[2], D, F)
+  sa1 = local_search(n, s1[0], s1[2], D, F)
   sa = acceptance_criterion(sa, sa1, sa[0], criteria_flag)
   while history.count(1) > 1 :
-    s1 = perturbation(n, sa[0], sa[2], dip, F)
-    sa1 = local_search(n, s1[0], s1[2], dip, F)
+    s1 = perturbation(n, sa[0], sa[2], D, F)
+    sa1 = local_search(n, s1[0], s1[2], D, F)
     sa = acceptance_criterion(sa, sa1, sa[0], criteria_flag)
     history = sa[0]
-
     list_plot.append(sa[1])
-
 #         cost  result[]  plot[]  
   return (sa[1], sa[2], list_plot)
     
@@ -189,10 +155,9 @@ def timer():
 # execution tests
 def execute_mean_test(cicles, path, criteria_flag):
     n, F, D = load_matrices(path)
-    dip = permutation(n, np.arange(n), D) #distance identity permutation -> this one will not change
    
     start = timer()
-    result = ils(n, dip, F, criteria_flag)
+    result = ils(n, D, F, criteria_flag)
     end = timer()
 
     mean = result[2]
@@ -203,10 +168,9 @@ def execute_mean_test(cicles, path, criteria_flag):
     i=1
     while i < cicles:
             n, F, D = load_matrices(path)
-            dip = permutation(n, np.arange(n), D) 
 
             start = timer()
-            result = ils(n, dip, F, criteria_flag)
+            result = ils(n, D, F, criteria_flag)
             end = timer()
 
             time = end - start
@@ -218,7 +182,7 @@ def execute_mean_test(cicles, path, criteria_flag):
             mean = [x / 2 for x in mean]
             mean_cost = (mean_cost + result[0]) / 2
 
-            print(i)
+            print("cicle: " + str(i) +" m_size:" +  str(n) + " criteria: " + str(criteria_flag))
             i += 1
     return (best_cost, best_result, mean, mean_time, mean_cost)                
  
@@ -227,7 +191,7 @@ def execute_mean_test(cicles, path, criteria_flag):
 
 local_path = os.getcwd()
 file_names = os.listdir("./qpa")
-results_dir = local_path + os.path.sep + "results"
+results_dir = local_path + os.path.sep + "results_e_imgs"
 result_file = results_dir + os.path.sep + "results.txt"
 try: 
     os.mkdir(results_dir)
@@ -239,8 +203,8 @@ else:
         a = 0
         #FOR EACH ACCEPTANCE CRITERIA 0=BETTER 1=RW 2=LSMC
         while a < 3:
-            #MEAN RESULTS OF 50 TESTS
-            number_of_tests = 50
+            #MEAN RESULTS OF 30 TESTS
+            number_of_tests = 30
             res = execute_mean_test(number_of_tests, path, a) 
            
             t_color = ('blue' if a == 0 else ('red' if a == 1 else 'green')) 
@@ -272,15 +236,8 @@ else:
             bash_cmd += " && echo \"\"  >> " + result_file
             
             error = subprocess.run(bash_cmd, shell=True, check=True, text=True)        
-            #print(error)
-
             a += 1 
 
         plt.savefig(results_dir + os.path.sep + "plot_" + os.path.splitext(problem_file)[0] + ".png" , bbox_inches='tight')
         plt.clf()
     print("end")
-
-
-
-
-
